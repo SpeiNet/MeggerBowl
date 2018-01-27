5477a10d0708b720364d934d37dec62c347263a8
Tobias Lückel <itsmegger@gmail.com>
2018-01-27 15:33:15 +0100

    Add a GoalScene with example scripts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Vector3 offset;
+	public GameObject player;
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+		ball.transform.position = transform.position;
+		player.transform.position = transform.position - offset;
+	}
+}
