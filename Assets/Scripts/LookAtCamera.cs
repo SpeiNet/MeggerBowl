18067a717ac2ba15cb4f8b25158bcb5dcb8b590d
Matthias Alt <mattagohni@gmail.com>
2018-01-27 15:41:21 +0100

    added some Assets

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void LateUpdate()
+	{
+		transform.LookAt (target.transform);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
