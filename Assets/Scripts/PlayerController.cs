0b990a2320212bf212fafaba10cda186b0edf15b
Matthias Alt <mattagohni@gmail.com>
2018-02-10 22:23:48 +0100

    reworked the jump mechanic to be a bit more realistic.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float fallMultiplyer = 2.5f;
+    public float lowJumpMultiplyer = 2f;
+
     public float speed;
     public float pullForce;
+
+    [Range(0, 10)]
 	public float jumpHeight;
 
     private Rigidbody rb;
 	private int jumpCount = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       
+        if (Input.GetButtonDown ("Jump") && jumpCount < 2) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+            jumpCount++;
+        }
+
+        if (rb.velocity.y < 0) {
+            // we are falling
+            rb.AddForce(Vector3.up * Physics.gravity.y * (fallMultiplyer - 1) * Time.deltaTime);
+        }
+
+        rb.AddForce(GetMovementViaInput() * speed);
+
+        PullGameBallIfFire2Pressed();
+
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Feuer Frei");
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            Debug.Log("Feuer Zwei");
+        }
+
+        if (Input.GetButton ("Fire3")) {
+            Debug.Log ("Feuer3");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log ("onCollison");
+        if (collision.collider.tag == "Stadium") {
+            Debug.Log ("hit rock bottom");
+            jumpCount = 0;
+        }
+    }
 
     private Vector3 GetMovementViaInput()
     {
@@ -30,44 +81,4 @@ public class PlayerController : MonoBehaviour
             gameBall.transform.GetComponent<Rigidbody>().velocity += pullDirection * (pullForce * Time.deltaTime);
         }
     }
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        rb.AddForce(GetMovementViaInput() * speed);
-
-        PullGameBallIfFire2Pressed();
-
-        if (Input.GetButton("Fire1"))
-        {
-            Debug.Log("Feuer Frei");
-        }
-
-        if (Input.GetButton("Fire2"))
-        {
-            Debug.Log("Feuer Zwei");
-        }
-
-		if (Input.GetButton ("Fire3")) {
-			Debug.Log ("Feuer3");
-		}
-
-		if (Input.GetButtonDown ("Jump") && jumpCount < 2) {
-			rb.AddForce (jumpHeight * Vector3.up);
-			jumpCount++;
-		}
-    }
-
-	void OnCollisionEnter(Collision collision)
-	{
-		Debug.Log ("onCollison");
-		if (collision.collider.tag == "Stadium") {
-			Debug.Log ("hit rock bottom");
-			jumpCount = 0;
-		}
-	}
 }
